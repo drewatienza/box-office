5c3765f19f1ab594bf82e9c5720a3120f1429873
drewatienza <atienza.drew@gmail.com>
2020-12-20 16:43:41 -0800

    Updates

---
+++ precache-manifest.75ce6a40b50f82fc289252148e39074b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff2b04f1f8caaae0f8de949be3519a2",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "ff124209a969166c406b",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "b15b51b39fe067c2b18c",
+    "url": "/box-office/static/js/2.419b6b4a.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/box-office/static/js/2.419b6b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff124209a969166c406b",
+    "url": "/box-office/static/js/main.09c4dd2c.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
